fd120156b18cbd23be78ae8ca66cb1091861673f
annie-sharkey <sharkeyconwaya@gmail.com>
2020-05-19 14:02:37 -0400

    App.js comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Quote from "./Quote.js";
 
+/*Another way to write state in a class component is without constructor, super, etc. 
+Now you can actually just say state = { }; and React knows what that means.
+You can use either but just so you know what's going on if you ever see it in code.
+*/
 class App extends Component {
   constructor(props) {
     super(props)
@@ -22,7 +26,15 @@ class App extends Component {
       fade: true
     })
   }
+/*
+The fade part of this is really cool. Keep it in your project.
+The following is just so you have this top of mind when designing for customers --
+the fade might make it difficult to read the quote particularly for someone who is visually
+impaired. 
 
+I like how you chose to split your components / made the decision to pass fade, number,
+and handleGenerate as props. Good structure. 
+*/
   render() {
     return (
       <div>
